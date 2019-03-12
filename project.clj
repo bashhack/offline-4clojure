22ed51156c241b90da407cfb7ef418db926ff875
bashhack (Marc Laughton) <info@marclaughton.com>
2019-03-12 03:07:30 -0400

    Add project.clj

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject offline-4clojure "0.1.0-SNAPSHOT"
+  :description "Offline-first 4Clojure problem set for convenient study"
+  :url "https://github.com/bashhack/offline-4clojure"
+  :license {:name "Eclipse Public License"
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [clj-http "3.9.1"]
+                 [cheshire "5.8.1"]
+                 [org.clojure/math.combinatorics "0.1.4"]]
+  :main  offline-4clojure.core)
